39e73fe653214f32d0f40be20db10d26edd202c3
Abdillah Faisal Nur Fajar <54137555+fajar-abdillah@users.noreply.github.com>
2019-08-31 06:10:15 +0700

    Create UVSens.ino

---
+++ C Project/UVSens/UVSens.ino
@@ -0,0 +1,25 @@
+double sensorValue, lux;
+int analogValue;
+
+void setup() 
+{
+  pinMode(A7, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() 
+{
+  analogValue = analogRead(A7);
+  sensorValue = (analogRead(A7) * (5.0 / 1023.0));
+  lux = sensorValue * 1333;
+
+  Serial.print("Digital Photodiode = ");
+  Serial.print(analogValue);
+  Serial.print("  |  Voltage = ");
+  Serial.print(sensorValue);
+  Serial.println(" mV");
+
+  Serial.print("Lux = ");
+  Serial.println(lux); 
+  delay(500);        
+}
